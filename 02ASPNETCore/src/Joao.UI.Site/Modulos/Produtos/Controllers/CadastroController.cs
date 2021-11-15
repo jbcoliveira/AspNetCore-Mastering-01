96b2ed2dbe182497343bf19fcc999bd50e3b4a27
jbcoliveira <jbcoliveira1984@gmail.com>
2021-11-14 21:35:53 -0300

    Areas, bundling e minification

---
+++ 02ASPNETCore/src/Joao.UI.Site/Modulos/Produtos/Controllers/CadastroController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Joao.UI.Site.Modulos.Produtos.Controllers
+{
+    [Area("Produtos")]
+    [Route("produtos")]
+    public class CadastroController : Controller
+    {
+        [Route("lista")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("busca")]
+        public IActionResult Busca()
+        {
+            return View();
+        }
+    }
+}
